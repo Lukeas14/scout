e80f7edb0902df26a201a5bbb881a63a94e377f7
Justin Lucas <Lukeas14@gmail.com>
2013-05-01 23:01:25 -0700

    Adding static files

--- scout_heroku/urls.py
+++ scout_heroku/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,6 +7,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^$', 'interviews.views.home'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     # Examples:
     # url(r'^$', 'scout_heroku.views.home', name='home'),
     # url(r'^scout_heroku/', include('scout_heroku.foo.urls')),
