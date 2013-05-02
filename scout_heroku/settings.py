70b192317a7b6d5649854ba3f7b6bb738dbce868
Justin Lucas <Lukeas14@gmail.com>
2013-05-01 23:24:47 -0700

    Added camera settings

--- scout_heroku/settings.py
+++ scout_heroku/settings.py
@@ -9,21 +9,21 @@ TEMPLATE_DEBUG = DEBUG
 PROJECT_DIR = Path(__file__).ancestor(2)
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+	# ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': '',                      # Or path to database file if using sqlite3.
+		# The following settings are not used with sqlite3:
+		'USER': '',
+		'PASSWORD': '',
+		'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+		'PORT': '',                      # Set to empty string for default.
+	}
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
@@ -74,17 +74,17 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    PROJECT_DIR.child("static"),
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	PROJECT_DIR.child("static"),
+	# Put strings here, like "/home/html/static" or "C:/www/django/static".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
@@ -93,19 +93,19 @@ SECRET_KEY = os.environ.get('SCOUT_SECRET_KEY')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	# Uncomment the next line for simple clickjacking protection:
+	# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'scout_heroku.urls'
@@ -114,25 +114,25 @@ ROOT_URLCONF = 'scout_heroku.urls'
 WSGI_APPLICATION = 'scout_heroku.wsgi.application'
 
 TEMPLATE_DIRS = (
-    PROJECT_DIR.child("templates"),
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	PROJECT_DIR.child("templates"),
+	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'south',
-    'interviews',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'south',
+	'interviews',
+	# Uncomment the next line to enable the admin:
+	# 'django.contrib.admin',
+	# Uncomment the next line to enable admin documentation:
+	# 'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -141,51 +141,58 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
+	'handlers': {
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django.request': {
+			'handlers': ['mail_admins'],
+			'level': 'ERROR',
+			'propagate': True,
+		},
+	}
 }
 
 SOCIAL_PROFILES_REQUIRED = 1
 
 #Environment specific settings
 if os.environ.get('SCOUT_ENV'):
-    SCOUT_ENV = os.environ.get('SCOUT_ENV')
+	SCOUT_ENV = os.environ.get('SCOUT_ENV')
 
-    if SCOUT_ENV == 'local':
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-                'NAME': 'scout',                      # Or path to database file if using sqlite3.
-                # The following settings are not used with sqlite3:
-                'USER': 'scout',
-                'PASSWORD': 'scout',
-                'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-                'PORT': '',                      # Set to empty string for default.
-            }
-        }
-    elif SCOUT_ENV == 'production':
-        import dj_database_url
-        DATABASES['default'] = dj_database_url.config()
+	if SCOUT_ENV == 'local':
+		DATABASES = {
+			'default': {
+				'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+				'NAME': 'scout',                      # Or path to database file if using sqlite3.
+				# The following settings are not used with sqlite3:
+				'USER': 'scout',
+				'PASSWORD': 'scout',
+				'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+				'PORT': '',                      # Set to empty string for default.
+			}
+		}
 
-        SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+		CAMERA_NAME = 'scout_dev'
+		CAMERA_UUID = '1dcf8440-93fd-0130-02a3-123139401838'
 
-        DEBUG = False
+	elif SCOUT_ENV == 'production':
+		import dj_database_url
+		DATABASES['default'] = dj_database_url.config()
+
+		SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+		DEBUG = False
+
+		CAMERA_NAME = 'scout_prod'
+		CAMERA_UUID = 'fd133d70-8de4-0130-d4dd-12313b086d5f'
