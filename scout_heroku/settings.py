4dac37749c66939a478629783ac77a5ec91da347
Justin Lucas <Lukeas14@gmail.com>
2013-05-01 23:27:43 -0700

    testing static secret key

--- scout_heroku/settings.py
+++ scout_heroku/settings.py
@@ -90,6 +90,7 @@ STATICFILES_FINDERS = (
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = os.environ.get('SCOUT_SECRET_KEY')
+SECRET_KEY = '2i32902j390j232903j902j3'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
